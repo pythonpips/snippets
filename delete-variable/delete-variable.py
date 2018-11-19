169c40d71c6a7c88a496d1ab2b2ec8bffa9c0dbb
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-11-19 19:59:17 +0530

    Add delete variable

---
+++ delete-variable/delete-variable.py
@@ -0,0 +1,10 @@
+"""author: @pythonpips"""
+
+foo = 'Hello Pythonista'
+del foo
+print(foo)
+"""This will outputs:
+Traceback (most recent call last):
+File "<stdin>", line 1, in <module>
+NameError: name 'foo' is not defined
+"""
