1ea69de811be7b64d007bf9d5a23061009b71731
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-11-22 16:32:02 +0530

    add dict keys

---
+++ dict-keys/dict-keys.py
@@ -0,0 +1,6 @@
+    """author: @pythonpips"""
+
+    contact = {'name': 'Vaibhav', 'phone': '+111111111111', 'skills': 'Python'}
+    contact_keys = [*contact]
+    print(contact_keys)
+    # contact_keys outputs ['name', 'phone', 'skills']
