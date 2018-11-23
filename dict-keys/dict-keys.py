2ccc4b2eeebe9c30ff572d4873f0cc801d425863
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-11-23 20:57:40 +0530

    fix indentation in dict-keys

--- dict-keys/dict-keys.py
+++ dict-keys/dict-keys.py
@@ -1,6 +1,6 @@
-    """author: @pythonpips"""
+"""author: @pythonpips"""
 
-    contact = {'name': 'Vaibhav', 'phone': '+111111111111', 'skills': 'Python'}
-    contact_keys = [*contact]
-    print(contact_keys)
-    # contact_keys outputs ['name', 'phone', 'skills']
+contact = {'name': 'Vaibhav', 'phone': '+111111111111', 'skills': 'Python'}
+contact_keys = [*contact]
+print(contact_keys)
+# contact_keys outputs ['name', 'phone', 'skills']
