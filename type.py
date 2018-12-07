28a59638f9f09999b01d5fadb478bb4c1f098e89
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-12-07 10:47:58 +0530

    add how to check data type in Python

---
+++ type.py
@@ -0,0 +1,9 @@
+"""author: @pythonpips"""
+
+is_python_awesome = True
+print(type(is_python_awesome))
+# outputs: <class 'bool'>
+
+greeting = "Hello, World!"
+print(type(greeting))
+# outputs: <class 'str'>
