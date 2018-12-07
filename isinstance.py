a66719a079baa163c4b92dda53d774abdd61abd4
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-12-07 11:04:44 +0530

    add isinstance.py

---
+++ isinstance.py
@@ -0,0 +1,9 @@
+"""author: @pythonpips"""
+
+is_python_awesome = True
+print(isinstance(is_python_awesome, bool))
+# outputs: True
+
+greeting = "Hello, World!"
+print(isinstance(greeting, int))
+# outputs: False  
