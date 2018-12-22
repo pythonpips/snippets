920877c8f902fe5cdc6e68e2fb7b62e706f44cdc
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-12-22 11:18:40 +0530

    add multiline string in Pythong

---
+++ multiline-string.py
@@ -0,0 +1,7 @@
+"""author: @pythonpips"""
+
+s = """Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt
+    ut labore."""
+# Multiline string are created with triple qoutes
